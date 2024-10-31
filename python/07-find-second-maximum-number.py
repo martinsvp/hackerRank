b66c1a6d83dff1b96362bc2ca51ae8b26d195d85
Victor Pedro <vpedro45@gmail.com>
2024-10-31 00:21:48 -0300

    feat: update files python folder

--- python/07-find-second-maximum-number.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-numbers = list(map(int, input().split()))
-
-max_number = max(numbers)
-numbers = [num for num in numbers if num != max_number]
-
-runner_up = max(numbers)
-print(runner_up)
